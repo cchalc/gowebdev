0033428be7c73697a909d45ac9652fc0de14beca
Christopher Chalcraft <chris.chalcraft@gmail.com>
2023-03-21 00:16:02 -0700

    added contactHandler

--- lenslocked/go.mod
+++ lenslocked/go.mod
@@ -1,3 +1,8 @@
 module example.com/lenslocked
 
 go 1.19
+
+require (
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:nKzQ5@example.com\">nKzQ5@example.com</a></p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
