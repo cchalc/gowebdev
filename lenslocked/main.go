8d559c230820a2984e5f2f95555216dd5f68d657
Christopher Chalcraft <chris.chalcraft@gmail.com>
2023-03-29 00:08:53 -0700

    adding path handler

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -15,8 +15,13 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:nKzQ5@example.com\">nKzQ5@example.com</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Path: ", r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
