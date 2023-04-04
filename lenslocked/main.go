14386076a4822f858b704ffa3206f5e46b0a1982
Christopher Chalcraft <chris.chalcraft@gmail.com>
2023-04-03 23:19:44 -0700

    update to handlerfunc

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -29,21 +29,23 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Router struct {}
+// type Router struct {}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
 
 func main() {
-  var router Router
+  // var router Router
+  var router http.HandlerFunc
+  router = pathHandler
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", router)
 
